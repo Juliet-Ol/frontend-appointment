33a12aafd976a6bbf5301301469efdce1aac8eb0
Juliet <agutu@gmail.com>
2022-06-12 17:35:19 +0300

    routing added automatically to module.tsafter creating components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppointmentFormComponent } from './appointment-form/appointment-form.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppointmentFormComponent,
+    NavbarComponent,
+    RegistrationComponent,
+    LoginComponent,
+    ProfileComponent,
+    LandingpageComponent
   ],
   imports: [
     BrowserModule,
